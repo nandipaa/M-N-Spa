7b7b7400dde8fc692ec8be185508cdba6e0b1a80
munashe <nandipa.mungadze@icloud.com>
2020-12-21 18:15:52 +0200

    commit message

--- Booking System/entertainment/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class EntertainmentConfig(AppConfig):
-    name = 'entertainment'
--- Sally's Refurbished Goods/goods/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Clothes
-
-
-class ClothesAdmin(admin.ModelAdmin):
-    list_display = ('name', 'image_url', 'price')
-
-
-admin.site.register(Clothes, ClothesAdmin)
